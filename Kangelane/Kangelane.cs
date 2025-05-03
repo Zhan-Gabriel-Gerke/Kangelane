ea66d0891299baa1284fea168dc1fc4a42622bc1
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-05-03 13:20:07 +0300

    Add project files.

---
+++ Kangelane/Kangelane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelane
+{
+    class Kangelane
+    {
+        private string nimi;
+        private string asukoht;
+    }
+}
