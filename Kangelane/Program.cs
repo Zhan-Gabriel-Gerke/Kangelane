5f26b93d45da2e59e3bc8bae2063c1ffdf280e23
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-05-03 13:36:05 +0300

    Save

--- Kangelane/Program.cs
+++ Kangelane/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Kangelane
+{
+    private string nimi;
+    private string asukoht;
+
+    public string Nimi
+    {
+        get { return nimi; }
+        set
+        {
+            if (value.Length > 2)
+                nimi = value;
+        }
+    }
+    public string Asukoht
+    {
+        get { return asukoht; }
+        set
+        {
+            if (value == "Tallnn" || value == "Tartu" || value == "Narva")
+                asukoht = value;
+        }
+    }
+}
